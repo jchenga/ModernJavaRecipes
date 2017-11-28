e2a5d9b9d1f5704060f5bf8293ddb7a6b96d21c9
Jack Cheng <j1cheng@bbyfs.ca>
2017-11-27 17:31:19 -0800

    streams

---
+++ src/mapsvsflatmap/FlatMapDemo.java
@@ -0,0 +1,50 @@
+package mapsvsflatmap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FlatMapDemo {
+    public static void main(String[] args) {
+        Customer sheridan = new Customer("Sheridan");
+        Customer ivanova = new Customer("Ivanova");
+        Customer garibaldi = new Customer("Garibaldi");
+
+        sheridan.addOrder(new Order(1))
+                .addOrder(new Order(2))
+                .addOrder(new Order(3));
+        ivanova.addOrder(new Order(4))
+                .addOrder(new Order(5));
+
+        List<Customer> customers = Arrays.asList(sheridan, ivanova, garibaldi);
+
+        // map for 1-1 customer to name --> Stream<String>
+        customers.stream()
+                .map(Customer::getName)
+                .forEach(System.out::println);
+
+        // map 1-many customer to orders --> Stream<List<Order>>
+        customers.stream()
+                .map(Customer::getOrders)
+                .forEach(System.out::println);
+
+        // map 1-many customer to orders.stream() --> Stream<Stream<Order>>
+        customers.stream()
+                .map(customer -> customer.getOrders().stream())
+                .forEach(System.out::println);
+
+        // stream() on an empty collection is already an empty stream
+        customers.stream()
+                .flatMap(customer -> customer.getOrders().stream())
+                .forEach(System.out::println);
+
+        // flatMap 1-many customer to orders.stream() --> Stream<Order>
+        // Note: extra detail included just for illustration;
+        //      stream() on an empty collection already returns an empty stream
+        customers.stream()
+                .flatMap(customer ->
+                            customer.getOrders().size() == 0 ? Stream.empty() :
+                                    customer.getOrders().stream())
+                .forEach(System.out::println);
+    }
+}
---
+++ src/streams/LazyStreams.java
@@ -0,0 +1,31 @@
+package streams;
+
+import java.util.stream.IntStream;
+
+public class LazyStreams {
+
+    public static int multByTwo(int n) {
+        System.out.printf("Inside multByTwo with arg %d%n", n);
+        return n * 2;
+    }
+
+    public static boolean modByThree(int n) {
+        System.out.printf("Inside modByThree with arg %d%n", n);
+        return n % 3 == 0;
+    }
+
+    public static void main(String[] args) {
+        int firstEvenDoubleDivBy3 = IntStream.range(100, 200)
+                                        .map(n -> n * 2)
+                                        .filter(n -> n % 3 == 0)
+                                        .findFirst().orElse(0);
+        System.out.println(firstEvenDoubleDivBy3);
+
+        // Demonstrate laziness using print statements
+        firstEvenDoubleDivBy3 = IntStream.range(100, 2_000_000)
+                                  .map(LazyStreams::multByTwo)
+                                  .filter(LazyStreams::modByThree)
+                                  .findFirst().orElse(0);
+        System.out.printf("First even divisible by 3 is %d%n", firstEvenDoubleDivBy3);
+    }
+}
