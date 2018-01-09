e1838df529d6c33d5537dbc73e862760f9afd8db
Jack Cheng <j1cheng@bbyfs.ca>
2018-01-08 16:47:21 -0800

    java.time

---
+++ tests/datetime/PaydayAdjusterTest.java
@@ -0,0 +1,35 @@
+package datetime;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class PaydayAdjusterTest {
+    @Test
+    public void payDay() {
+        TemporalAdjuster adjuster = new PaydayAdjuster();
+        IntStream.rangeClosed(1, 14)
+                .mapToObj(day -> LocalDate.of(2017, Month.JULY, day))
+                .forEach(date -> assertEquals(14, date.with(adjuster).getDayOfMonth()));
+        IntStream.rangeClosed(15, 31)
+                .mapToObj(day -> LocalDate.of(2017, Month.JULY, day))
+                .forEach(date -> assertEquals(31, date.with(adjuster).getDayOfMonth()));
+    }
+
+    @Test
+    public void payDayWithMethodRef() {
+        IntStream.rangeClosed(1,14)
+                .mapToObj(day -> LocalDate.of(2017, Month.JULY,day))
+                .forEach(date -> assertEquals(14, date.with(Adjusters::adjustInfo).getDayOfMonth()));
+
+        IntStream.rangeClosed(15,31)
+                .mapToObj(day -> LocalDate.of(2017, Month.JULY, day))
+                .forEach(date -> assertEquals(31, date.with(Adjusters::adjustInfo).getDayOfMonth()));
+    }
+}
